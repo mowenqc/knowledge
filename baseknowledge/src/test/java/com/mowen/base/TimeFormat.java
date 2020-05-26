6d7f87a9879e740b84b0aad605a12ff309778357
mowenqc <mowenqc@163.com>
2020-05-26 22:47:15 +0800

    SimpleDateFormat进行时间的格式化

---
+++ baseknowledge/src/test/java/com/mowen/base/TimeFormat.java
@@ -0,0 +1,18 @@
+package com.mowen.base;
+
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeFormat {
+    @Test
+    public void testTimeFormat(){
+        /**
+         * 注意一点非常重要的就是：SimpleDateFormat是线程不安全的，不要定义为全局变量，我们在需要的地方，每次都new一个。
+         */
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-HH");
+        String format = sdf.format(new Date());
+        System.out.println(format);
+    }
+}

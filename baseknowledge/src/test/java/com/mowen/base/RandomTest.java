236009ad9c4b7ab63e77552cb3981c256810074d
huangkai <420969651@qq.com>
2020-05-27 19:09:53 +0800

    测试随机数

--- baseknowledge/src/test/java/com/mowen/base/RandomTest.java
+++ baseknowledge/src/test/java/com/mowen/base/RandomTest.java
@@ -2,7 +2,9 @@ package com.mowen.base;
 
 import org.junit.Test;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 /****
  * @project: knowledge_parent
@@ -25,6 +27,23 @@ public class RandomTest {
     public void testMathRandom(){
         for (int i = 0 ; i< 10;i++){
             System.out.println((int) (Math.random() * 10));
+        }
+    }
+
+    @Test
+    public void testGenerateRandomList(){
+        int i = 1;
+        Random random = new Random();
+        Set<Integer> setList = new HashSet<>();
+        int j = 0;
+        while (true){
+            if(setList.size() == 100){
+                break;
+            }
+           /* random.setSeed(i++);*/
+            int intType = random.nextInt(2000);
+            setList.add(intType);
+            System.out.println(j++);
         }
     }
 }

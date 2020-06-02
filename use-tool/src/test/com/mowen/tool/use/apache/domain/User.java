bed7b8292f469b49ec195eca8c9e106305a0451a
huangkai <420969651@qq.com>
2020-06-02 13:04:13 +0800

    common-lang format

---
+++ use-tool/src/test/com/mowen/tool/use/apache/domain/User.java
@@ -0,0 +1,20 @@
+package com.mowen.tool.use.apache.domain;
+
+import lombok.Data;
+
+/****
+ * @project: knowledge_parent
+ * @author: huangkai
+ * @create-time: 2020/6/2 10:39
+ * @description:
+ *****/
+@Data
+public class User {
+
+    private int id;
+    private String name;
+
+    private int age;
+
+
+}

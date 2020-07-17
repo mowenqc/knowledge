c557cf2b2a6bee451b3d39d7cb26dfa088a454ff
huangkai <420969651@qq.com>
2020-07-17 17:50:10 +0800

    堆排序

--- algorithm/src/main/java/cn/mowenqc/algorithm/sort/HeapSort.java
+++ algorithm/src/main/java/cn/mowenqc/algorithm/sort/HeapSort.java
@@ -12,7 +12,12 @@ public class HeapSort extends BaseSort {
     public static void main(String[] args) {
         int []array = getInitArray();
         printArray(array);
-
+        //构建小根堆
+        buildMinHeapSort(array);
         printArray(array);
+    }
+
+    private static void buildMinHeapSort(int []array){
+
     }
 }
